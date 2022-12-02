f2dbae0ec28c068e57004da743f9cc4c28cfb2cd
Nedd Chairi <neddyjob@gmail.com>
2022-12-02 19:25:35 +0100

    minor bug fixes

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import os
-import pyaudio
 import speech_recognition as sr
 
 
 def nombreArchivo():
-    # de qué clase que era el audio
     materia = input("¿De qué asignatura era el audio?: ")
-
-    # fecha de la clase
     fecha = input("¿Cuando se realizó esa clase? (dd.mm.aaaa): ")
-
-    # mostrar nombre final del archivo
     nombreFinal = f"{materia} - {fecha}"
     print(f"El nombre del archivo final será {nombreFinal}.txt")
     confirmacion = input("¿Estás de acuerdo? S/N: ")
@@ -35,7 +28,6 @@ def transformarAudioEnTexto(rutaArchivo):
 
 
 def crearArchivoTexto(nombreFinal, texto):
-    print(nombreFinal)
     archivo = open(f"{nombreFinal}.txt", "w")
     archivo.write(str(texto))
     archivo.close()
