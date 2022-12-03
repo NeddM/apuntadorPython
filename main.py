1b7c8d98cc27666018649f46c9f73bbdbb621678
Nedd Chairi <neddyjob@gmail.com>
2022-12-03 12:10:53 +0100

    Ahora se pueden transcribir audios de más de 1 min

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+
+from os import remove
+import math
+from operator import contains
 import speech_recognition as sr
+from pydub import AudioSegment
 
 
 def nombreArchivo():
@@ -17,18 +22,36 @@ def cargarArchivo():
     return nombreArchivoBruto
 
 
-def transformarAudioEnTexto(rutaArchivo):
-    r = sr.Recognizer()
+def transformarAudioEnTexto(rutaArchivo, nombreFinal):
+    seg = 50
 
-    archivo = sr.AudioFile(rutaArchivo)
-    with archivo as origen:
-        audio = r.record(origen)
-        texto = r.recognize_google(audio, language='es')
-        return texto
+    speech = AudioSegment.from_wav(rutaArchivo)
+
+    batch_size = seg * 1000
+    duracion = speech.duration_seconds
+    batches = math.ceil(duracion / seg)
+
+    inicio = 0
+    for i in range(batches):
+        trozo = speech[inicio: inicio + batch_size]
+        trozo.export(f'trozo_{i}.wav', format='wav')
+        inicio += batch_size
+
+        r = sr.Recognizer()
+
+        archivo = sr.AudioFile(f"trozo_{i}.wav")
+        with archivo as origen:
+            audio = r.record(origen)
+            texto = r.recognize_google(audio, language='es')
+            archivo = open(f"{nombreFinal}.txt", "a")
+            archivo.write(str(texto))
+            archivo.close()
+
+        remove(f"trozo_{i}.wav")
 
 
 def crearArchivoTexto(nombreFinal, texto):
-    archivo = open(f"{nombreFinal}.txt", "w")
+    archivo = open(f"{nombreFinal}.txt", "a")
     archivo.write(str(texto))
     archivo.close()
 
@@ -36,8 +59,7 @@ def crearArchivoTexto(nombreFinal, texto):
 def main():
     rutaArchivo = cargarArchivo()
     nombreFinal = nombreArchivo()
-    texto = transformarAudioEnTexto(rutaArchivo)
-    crearArchivoTexto(nombreFinal, texto)
+    transformarAudioEnTexto(rutaArchivo, nombreFinal)
 
 
 if __name__ == "__main__":
